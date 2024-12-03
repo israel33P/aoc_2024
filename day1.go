834fc7147c49315550085b13ecc52e4ce39b60b1
Israel <israel.sanchez@cnn.com>
2024-12-03 13:42:54 -0500

    updated day 1, ran into sort go version issue

---
+++ day1.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	s "strings"
+	"fmt"
+	"os"
+	"strconv"
+	"slices"
+)
+
+func eCheck(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func textReader(filePath string) string {
+	dat, err := os.ReadFile(filePath)
+	eCheck(err)
+
+	return string(dat)
+}
+
+func converter(inputText string) ([]int, []int) {
+	lines := s.Split(inputText, "\n")
+	var lCount int = 0
+	var rCount int = 0
+	var leftArr []int
+	var rightArr []int
+	leftArr = make([]int, len(lines))
+	rightArr = make([]int, len(lines))
+	for _, l := range lines {
+		vals := s.Split(l, " ")
+		lVal := s.TrimSpace(vals[0])
+		rVal := s.TrimSpace(vals[len(vals)-1])
+		if lVal != ""{
+			i, err := strconv.Atoi(lVal)
+			eCheck(err)
+			leftArr[lCount] = i
+			lCount++
+		}
+		if rVal != ""{
+			i, err := strconv.Atoi(rVal)
+			eCheck(err)
+			rightArr[rCount] = i
+			rCount++
+		}
+	}
+	slices.Sort(leftArr)
+	slices.Sort(rightArr)
+	return leftArr, rightArr
+}
+
+func main() {
+	inputText := textReader("/mnt/c/Users/isrsanchez/Documents/_Scripting/AdventOfCode_2024/inputs/input_day1.txt")
+	leftArr, rightArr := converter(inputText)
+
+	fmt.Print(leftArr)
+	fmt.Println("\n=========================================")
+	fmt.Println(rightArr)
+}
